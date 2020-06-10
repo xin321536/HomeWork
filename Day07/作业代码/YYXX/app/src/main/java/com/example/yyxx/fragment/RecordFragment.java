c7b05a454e4a71f653cad53e9cc0d3607ad2d4a1
Thierry1 <floveandroid@sina.com>
2020-06-10 08:07:09 +0800

    m

---
+++ Day07/作业代码/YYXX/app/src/main/java/com/example/yyxx/fragment/RecordFragment.java
@@ -0,0 +1,20 @@
+package com.example.yyxx.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.yyxx.R;
+
+public class RecordFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_record, container, false);
+    }
+}
